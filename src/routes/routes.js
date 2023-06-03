cd2f5119d8fd077fcafa79139407fdee8a8b6465
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-06-03 15:42:26 +0600

    packages integrated

---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: "welcome to the home page"
+    }
+])
+
+export default routes;
