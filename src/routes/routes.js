6d352adffea47d8b8e63b6a5bf50954482dacc8c
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-06-03 16:12:52 +0600

    route added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/main.layout";
 
 const routes = createBrowserRouter([
     {
         path: "/",
-        element: "welcome to the home page"
+        element: <MainLayout/>,
+        children: [
+            {
+                path: "/",
+                element: <h1>Home</h1>
+            }
+        ]
     }
 ])
 
