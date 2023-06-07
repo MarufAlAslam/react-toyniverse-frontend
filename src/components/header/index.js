4bde603d7b0fae04dc0ad558e5b891e63ba9af1d
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-06-07 12:44:00 +0600

    header done

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,34 +1,40 @@
 import React from "react";
 import logo from "../../assets/images/logo.png";
-import { Link, NavLink } from "react-router-dom";
-import avatar from "../../assets/images/avatar.png"
+import { NavLink } from "react-router-dom";
+import avatar from "../../assets/images/avatar.png";
 
 const Header = () => {
   return (
     <div className="px-6 py-2 header flex justify-between items-center bg-[#0f674d]">
       <NavLink className={"flex justify-start items-center"} to="/">
         <img src={logo} alt="logo" className="w-[60px] mr-1" />
-        <span className="text-xl font-bold text-[#fff] uppercase">toyniverse</span>
+        <span className="text-xl font-bold text-[#fff] uppercase">
+          toyniverse
+        </span>
       </NavLink>
       <div className="w-full justify-end items-center flex space-x-10">
-        <NavLink to={'/'} className={"text-white"}>
-            Home
+        <NavLink to={"/"} className={"text-white"}>
+          Home
         </NavLink>
-        <NavLink to={'/'} className={"text-white"}>
-            All Toys
+        <NavLink to={"/all-toys"} className={"text-white"}>
+          All Toys
         </NavLink>
-        <NavLink to={'/'} className={"text-white"}>
-            Blogs
+        <NavLink to={"/blogs"} className={"text-white"}>
+          Blogs
         </NavLink>
-        <NavLink to={'/'} className={"text-white"}>
-            My Toys
+        <NavLink to={"/my-toys"} className={"text-white"}>
+          My Toys
         </NavLink>
-        <NavLink to={'/'} className={"text-white"}>
-            Add a Toy
+        <NavLink to={"/add-toy"} className={"text-white"}>
+          Add a Toy
         </NavLink>
-        <Link>
-        <img src={avatar} alt="avatar" className="w-[40px] h-[40px] rounded-full bg-white p-1" />
-        </Link>
+        <NavLink to={'/profile'}>
+          <img
+            src={avatar}
+            alt="avatar"
+            className="w-[40px] h-[40px] rounded-full bg-white p-1"
+          />
+        </NavLink>
       </div>
     </div>
   );
